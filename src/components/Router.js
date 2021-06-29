03483e72a4cb27de012e2a1a77d4fef5daedc3b8
canine89 <phk707kr@gmail.com>
2021-06-29 17:38:42 +0900

    07-1까지 실습 완료

---
+++ src/components/Router.js
@@ -0,0 +1,31 @@
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import Auth from "routes/Auth";
+import Home from "routes/Home";
+import Profile from "routes/Profile";
+import Navigation from "./Navigation";
+
+const AppRouter = ({ isLoggedIn, userObj }) => {
+  return (
+    <Router>
+      {isLoggedIn && <Navigation />}
+      <Switch>
+        {isLoggedIn ? (
+          <>
+            <Route exact path="/">
+              <Home userObj={userObj} />
+            </Route>
+            <Route exact path="/profile">
+              <Profile userObj={userObj} />
+            </Route>
+          </>
+        ) : (
+          <Route exact path="/">
+            <Auth />
+          </Route>
+        )}
+      </Switch>
+    </Router>
+  );
+};
+
+export default AppRouter;
---
+++ src/routes/Profile.js
@@ -0,0 +1,34 @@
+import { authService, dbService } from "fbase";
+import { useEffect } from "react";
+import { useHistory } from "react-router-dom";
+
+const Profile = ({ userObj }) => {
+  const history = useHistory();
+
+  const onLogOutClick = () => {
+    authService.signOut();
+    history.push("/");
+  };
+
+  const getMyNweets = async () => {
+    const nweets = await dbService
+      .collection("nweets")
+      .where("creatorId", "==", userObj.uid)
+      .orderBy("createdAt", "asc")
+      .get();
+
+    console.log(nweets.docs.map((doc) => doc.data()));
+  };
+
+  useEffect(() => {
+    getMyNweets();
+  }, []);
+
+  return (
+    <>
+      <button onClick={onLogOutClick}>Log Out</button>
+    </>
+  );
+};
+
+export default Profile;
